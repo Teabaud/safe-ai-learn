746908f526825964a4a1e15f6f5f904e37bbdf03
Thibaud Veron <thibaud.veron@free.fr>
2025-02-02 00:59:15 +0100

    Handling missing translations

--- app/[locale]/lessons/[id]/page.tsx
+++ app/[locale]/lessons/[id]/page.tsx
@@ -3,6 +3,8 @@ import LessonCheckList from "@/components/lessons/checklist";
 import LessonSummary from "@/components/lessons/summary";
 import LessonResource from "@/components/lessons/resources";
 import LessonQuizz from "@/components/lessons/quizz";
+import { getLocale, getTranslations } from "next-intl/server";
+import { Locale } from "@/locales.config";
 
 function getLesson(id: string): Lesson {
   console.log(`Fetching lesson with id: ${id}`);
@@ -16,6 +18,12 @@ interface LessonPageProps {
 export default async function LessonPage({ params }: LessonPageProps) {
   const lessonId = (await params).id;
   const lesson = getLesson(lessonId);
+  const locale = (await getLocale()) as Locale;
+  const t = await getTranslations("lessons");
+
+  if (!lesson.translations.hasOwnProperty(locale)) {
+    return <div>{t("noTranslation")}</div>;
+  }
 
   return (
     <div>
--- app/[locale]/lessons/page.tsx
+++ app/[locale]/lessons/page.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-
-import { sampleLesson } from "@/types/lesson";
+import { Lesson, sampleLesson } from "@/types/lesson";
+import LessonCheckList from "@/components/lessons/checklist";
 import LessonSummary from "@/components/lessons/summary";
 import LessonResource from "@/components/lessons/resources";
 import LessonQuizz from "@/components/lessons/quizz";
-import LessonCheckList from "@/components/lessons/checklist";
+import { getLocale, getTranslations } from "next-intl/server";
+import { Locale } from "@/locales.config";
 
-export default function LessonPage() {
+function getLesson(id: string): Lesson {
+  console.log(`Fetching lesson with id: ${id}`);
+  return sampleLesson;
+}
+
+interface LessonPageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function LessonPage({ params }: LessonPageProps) {
+  const lessonId = (await params).id;
+  const lesson = getLesson(lessonId);
+  const locale = (await getLocale()) as Locale;
+  const t = await getTranslations("lessons");
+
+  if (!lesson.translations.hasOwnProperty(locale)) {
+    return <div>{t("noTranslation")}</div>;
+  }
+
   return (
-    <div className="max-w-4xl mx-auto p-4 space-y-6">
+    <div>
       <LessonSummary lesson={sampleLesson} />
       <LessonResource lesson={sampleLesson} />
       <LessonQuizz lesson={sampleLesson} />
-      <LessonCheckList lesson={sampleLesson} />
+      <LessonCheckList lesson={lesson} />
     </div>
   );
 }
